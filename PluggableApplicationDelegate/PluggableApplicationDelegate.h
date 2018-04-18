0f22da2846fac1349ef9b8e2f4368268ca2ac768
Jakub Petrík <petrik@inloop.eu>
2018-04-18 10:15:50 +0200

    Initial Commit

---
+++ PluggableApplicationDelegate/PluggableApplicationDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PluggableApplicationDelegate.h
+//  PluggableApplicationDelegate
+//
+//  Created by Jakub Petrík on 4/18/18.
+//  Copyright © 2018 INLOOPX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PluggableApplicationDelegate.
+FOUNDATION_EXPORT double PluggableApplicationDelegateVersionNumber;
+
+//! Project version string for PluggableApplicationDelegate.
+FOUNDATION_EXPORT const unsigned char PluggableApplicationDelegateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PluggableApplicationDelegate/PublicHeader.h>
+
+
