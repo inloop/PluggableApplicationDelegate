301b153dd83934bf487ae2e9add84451d04e69ee
Jakub Petrík <petrik@inloop.eu>
2018-04-18 10:22:03 +0200

    FEAT: Add sources

--- PluggableApplicationDelegate/PluggableApplicationDelegate.h
+++ PluggableApplicationDelegate/PluggableApplicationDelegate.h
@@ -1,10 +1,4 @@
-//
-//  PluggableApplicationDelegate.h
-//  PluggableApplicationDelegate
-//
-//  Created by Jakub Petrík on 4/18/18.
 //  Copyright © 2018 INLOOPX. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
@@ -13,7 +7,3 @@ FOUNDATION_EXPORT double PluggableApplicationDelegateVersionNumber;
 
 //! Project version string for PluggableApplicationDelegate.
 FOUNDATION_EXPORT const unsigned char PluggableApplicationDelegateVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <PluggableApplicationDelegate/PublicHeader.h>
-
-
